5dc563c20437a9234304da6663686f5a5436d104
onhope <chch4934@gmail.com>
2024-08-29 19:34:01 +0900

    1. 오른쪽에 고정 메뉴 만들기

---
+++ app2.js
@@ -0,0 +1,38 @@
+const sections = document.querySelectorAll(".section");
+const secBtns = document.querySelectorAll(".controlls");
+const secBtn = document.querySelectorAll(".control");
+const allSections = document.querySelector(".main-content");
+
+function PageTransitions() {
+    // 버튼을 클릭을 하면 active class
+    for (let i = 0; i < secBtn.length; i++) {
+        secBtn[i].addEventListener("click", () => {
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('.active-btn', '');
+            this.className += ' active-btn';
+        })
+    }
+
+    allSections.addEventListener("click", (e) => {
+        const id = e.target.dataset.id;
+
+        if (id) {
+            secBtns.forEach((btn) => {
+                btn.classList.remove('active');
+            })
+            e.target.classList.add('active');
+
+            sections.forEach((section) => {
+                section.classList.remove("active");
+            });
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
+}
+
+PageTransitions();
+
+
+
